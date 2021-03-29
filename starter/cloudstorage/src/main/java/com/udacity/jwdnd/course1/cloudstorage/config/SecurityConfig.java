3b9847b931ddc2840318800615b967169c656b84
Administrador <contato.gabrielgarcia@gmail.com>
2021-03-29 03:09:21 -0300

    Adjstments

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/config/SecurityConfig.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/config/SecurityConfig.java
@@ -1,24 +1,27 @@
 package com.udacity.jwdnd.course1.cloudstorage.config;
 
-//@Configuration
-//@EnableWebSecurity
-//public class SecurityConfig extends WebSecurityConfigurerAdapter {
-//
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//        http
-//        .authorizeRequests()
-//        .antMatchers("/","/assets/**", "/users")
-//        .permitAll()
-//        .anyRequest()
-//        .authenticated()
-//        .and()
-//        .formLogin()
-//        .loginPage("/login")
-//        .defaultSuccessUrl("/home")
-//        .permitAll()
-//        .and()
-//        .logout()
-//        .permitAll();
-//    }
-//}
+import com.udacity.jwdnd.course1.cloudstorage.service.AuthenticationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private AuthenticationService authenticationService;
+
+    public SecurityConfig(AuthenticationService authenticationService) {
+        this.authenticationService = authenticationService;
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder authenticationManagerBuilder) {
+        authenticationManagerBuilder.authenticationProvider((AuthenticationProvider) this.authenticationService);
+    }
+
+}
---
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/service/AuthenticationService.java
@@ -0,0 +1,10 @@
+package com.udacity.jwdnd.course1.cloudstorage.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthenticationService {
+
+
+
+}
